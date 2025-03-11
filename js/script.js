3fe1e265ded6509146740d3bc73564d35c0de69a
Your Name <you@example.com>
2025-03-11 13:44:41 +0500

    feat: add offers about section

--- js/script.js
+++ js/script.js
@@ -38,153 +38,20 @@ $(document).ready(function () {
             },
   ]
       }
-    } else if (slider.hasClass('categories')) {
+    } else if (slider.hasClass('offers')) {
       extraOptions = {
         slidesToShow: 1,
           slidesToScroll: 1,
           centerMode: false,
           arrows: true,
+          dots: true,
           autoPlay: true,
-        responsive: [
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 2.7,
-        slidesToScroll: 1,
-      }
-          },
-    {
-      breakpoint: 680,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      }
-    },
-    {
-      breakpoint: 576,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }
-            },
-  ]
       }
     }
     slider.slick($.extend({}, extraOptions, options ));
   })
 })
 
-
-
-
-
-// $('.slider').slick({
-//     dots: false,
-//     arrows: true,
-//     infinite: true,
-//     speed: 300,
-//     slidesToShow: 3.5,
-//     slidesToScroll: 1,
-//     prevArrow: $('.prev-btn'), 
-//     nextArrow: $('.next-btn'),
-//     responsive: [
-//         {
-//             breakpoint: 1024,
-//             settings: {
-//                 slidesToShow: 3,
-//                 slidesToScroll: 1
-//             }
-//         },
-//         {
-//             breakpoint: 768,
-//             settings: {
-//                 slidesToShow: 1,
-//                 slidesToScroll: 1
-//             }
-//         },
-//         {
-//             breakpoint: 480,
-//             settings: {
-//                 slidesToShow: 1,
-//                 slidesToScroll: 1
-//             }
-//         }
-//     ]
-// });
-
-
-
-
-// $(document).ready(function(){
-//     $('.slider-offer').slick({
-//         dots: true,
-//         arrows: true,
-//         infinite: true,
-//         speed: 300,
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         prevArrow: $('.prev'), 
-//         nextArrow: $('.next'),
-//         responsive: [
-//             {
-//                 breakpoint: 1024,
-//                 settings: {
-//                     slidesToShow: 3,
-//                     slidesToScroll: 3
-//                 }
-//             },
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                     slidesToShow: 1,
-//                     slidesToScroll: 1
-//                 }
-//             },
-//             {
-//                 breakpoint: 480,
-//                 settings: {
-//                     slidesToShow: 1,
-//                     slidesToScroll: 1
-//                 }
-//             }
-//         ]
-//     });
-// });
-
-
-// $('.slider-tours').slick({
-//     dots: false,
-//     arrows: true,
-//     infinite: true,
-//     speed: 300,
-//     slidesToShow: 3.5,
-//     slidesToScroll: 1,
-//     prevArrow: $('.prv-btn'), 
-//     nextArrow: $('.nxt-btn'),
-//     responsive: [
-//         {
-//             breakpoint: 1024,
-//             settings: {
-//                 slidesToShow: 3,
-//                 slidesToScroll: 1
-//             }
-//         },
-//         {
-//             breakpoint: 768,
-//             settings: {
-//                 slidesToShow: 1,
-//                 slidesToScroll: 1
-//             }
-//         },
-//         {
-//             breakpoint: 480,
-//             settings: {
-//                 slidesToShow: 1,
-//                 slidesToScroll: 1
-//             }
-//         }
-//     ]
-// });
 
 
 AOS.init();
