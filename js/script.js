f36c51347be7d48b0bf10500e915ada992ab1d7f
Your Name <you@example.com>
2025-03-11 11:57:29 +0500

    fix: fixed tours-card section, chore: revrite slick-slider initialization

--- js/script.js
+++ js/script.js
@@ -1,110 +1,190 @@
-$('.slider').slick({
-    dots: false,
-    arrows: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 3.5,
-    slidesToScroll: 1,
-    prevArrow: $('.prev-btn'), 
-    nextArrow: $('.next-btn'),
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
-});
-
-
-
-
-$(document).ready(function(){
-    $('.slider-offer').slick({
-        dots: true,
-        arrows: true,
-        infinite: true,
-        speed: 300,
+$(document).ready(function () {
+    $('.slick-slider').each(function () {
+    let slider = $(this);
+    let options = {
+      prevArrow: slider.parent().find('.slider-prev'),
+      nextArrow: slider.parent().find('.slider-next'),
+      infinite: true,
+    }
+    let extraOptions = {}
+    if (slider.hasClass('tours-cards')) {
+      extraOptions = {
+        slidesToShow: 3,
+          slidesToScroll: 1,
+          centerMode: false,
+          arrows: true,
+          autoPlay: true,
+        responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2.5,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
-        prevArrow: $('.prev'), 
-        nextArrow: $('.next'),
+      }
+            },
+  ]
+      }
+    } else if (slider.hasClass('categories')) {
+      extraOptions = {
+        slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+          arrows: true,
+          autoPlay: true,
         responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3
-                }
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2.7,
+        slidesToScroll: 1,
+      }
+          },
+    {
+      breakpoint: 680,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
             },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
-});
+  ]
+      }
+    }
+    slider.slick($.extend({}, extraOptions, options ));
+  })
+})
 
 
-$('.slider-tours').slick({
-    dots: false,
-    arrows: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 3.5,
-    slidesToScroll: 1,
-    prevArrow: $('.prv-btn'), 
-    nextArrow: $('.nxt-btn'),
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
-});
+
+
+
+// $('.slider').slick({
+//     dots: false,
+//     arrows: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 3.5,
+//     slidesToScroll: 1,
+//     prevArrow: $('.prev-btn'), 
+//     nextArrow: $('.next-btn'),
+//     responsive: [
+//         {
+//             breakpoint: 1024,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 1
+//             }
+//         },
+//         {
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1
+//             }
+//         },
+//         {
+//             breakpoint: 480,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1
+//             }
+//         }
+//     ]
+// });
+
+
+
+
+// $(document).ready(function(){
+//     $('.slider-offer').slick({
+//         dots: true,
+//         arrows: true,
+//         infinite: true,
+//         speed: 300,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         prevArrow: $('.prev'), 
+//         nextArrow: $('.next'),
+//         responsive: [
+//             {
+//                 breakpoint: 1024,
+//                 settings: {
+//                     slidesToShow: 3,
+//                     slidesToScroll: 3
+//                 }
+//             },
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1
+//                 }
+//             },
+//             {
+//                 breakpoint: 480,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1
+//                 }
+//             }
+//         ]
+//     });
+// });
+
+
+// $('.slider-tours').slick({
+//     dots: false,
+//     arrows: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 3.5,
+//     slidesToScroll: 1,
+//     prevArrow: $('.prv-btn'), 
+//     nextArrow: $('.nxt-btn'),
+//     responsive: [
+//         {
+//             breakpoint: 1024,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 1
+//             }
+//         },
+//         {
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1
+//             }
+//         },
+//         {
+//             breakpoint: 480,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1
+//             }
+//         }
+//     ]
+// });
 
 
 AOS.init();
