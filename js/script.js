fecf5787c790cb778c0ac234c9f3c743fb10fb60
Your Name <you@example.com>
2025-03-11 16:04:40 +0500

    feat: add footer section

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
       prevArrow: slider.parent().find('.slider-prev'),
       nextArrow: slider.parent().find('.slider-next'),
       infinite: true,
+      autoPlay: true,
     }
     let extraOptions = {}
     if (slider.hasClass('tours-cards')) {
@@ -47,13 +48,45 @@ $(document).ready(function () {
           dots: true,
           autoPlay: true,
       }
+    }else if (slider.hasClass('gallery')) {
+      extraOptions = {
+        slidesToShow: 3,
+          slidesToScroll: 1,
+          centerMode: false,
+          arrows: true,
+          dots: false,
+          autoPlay: true,
+          responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 2.5,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 576,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+                    },
+          ]
+      }
     }
     slider.slick($.extend({}, extraOptions, options ));
   })
+  AOS.init();
 })
 
 
 
-AOS.init();
 
  // JavaScript to handle pagination
